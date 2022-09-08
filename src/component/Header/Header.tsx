a155a340ad79c8cfefd8b31440bc0225ef42532d
Chong <chongconan320@gmail.com>
2022-09-08 18:37:27 +0800

    Responsive on Header component

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -8,7 +8,10 @@ import { ReactComponent as ImageMarvelLogo } from "assets/images/marvel_logo.svg
 import NavButton from "./NavButton";
 
 // Import library
-import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Hamburger from "./Hamburger";
+import Overlay from "./Overlay";
 
 const navigation = [
   {
@@ -20,18 +23,52 @@ const navigation = [
     link: "/comics",
   },
   {
-    label: "creators",
+    label: "Creators",
     link: "/creators",
   },
 ];
 
 const Header = () => {
   const { pathname } = useLocation();
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const onToggle = () => {
+    setIsExpanded((prev) => !prev);
+  };
   return (
     <header className={styles["header"]}>
+      <Overlay isExpanded={isExpanded}>
+        <div className={styles["overlay"]}>
+          <ul
+            className={`${styles["nav__links"]} ${styles["nav__links--mobile"]}`}
+          >
+            {navigation.map(({ label, link }) => (
+              <li key={link}>
+                <Link
+                  to={link}
+                  onClick={onToggle}
+                  className={`${styles["link"]} ${
+                    pathname === link
+                      ? styles["link--selected"]
+                      : styles["link-inactive"]
+                  }`}
+                >
+                  {label}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </Overlay>
       <ImageMarvelLogo className={styles["header__logo"]} />
       <nav className={styles["header__nav"]}>
-        <ul>
+        <div className={styles["header__hamburger"]}>
+          <Hamburger isExpanded={isExpanded} onClick={onToggle} />
+        </div>
+
+        <ul
+          className={`${styles["nav__links"]} ${styles["nav__links--desktop"]}`}
+        >
           {navigation.map(({ label, link }) => (
             <li key={link}>
               <NavButton to={link} selected={link === pathname}>
