128f926d5716da9a0760c64f97d56f1d1eb41903
Chong <chongconan320@gmail.com>
2022-09-10 20:29:22 +0800

    Characters page and it's component

---
+++ src/pages/Characters/Characters.tsx
@@ -0,0 +1,51 @@
+import useGet, { Status } from "hooks/useGet";
+import { Avatar } from "component/Characters";
+import styles from "./characters.module.css";
+import { Collections, SearchBar } from "component/General";
+import SpinningWeb from "component/General/SpinningWeb";
+import { useState } from "react";
+import { ICharacters } from "types/characters";
+import { ICharactersRequest } from "types/api";
+
+const LIMIT = 8;
+
+const Characters = () => {
+  const [page, setPage] = useState(1);
+  const [options, setOptions] = useState<ICharactersRequest>({
+    offset: (page - 1) * LIMIT,
+    limit: LIMIT,
+  });
+
+  const [characters, status] = useGet<ICharacters, ICharactersRequest>(
+    "characters",
+    options
+  );
+  return (
+    <Collections>
+      {status === Status.complete && (
+        <div className={styles["collections__loading"]}>
+          <SpinningWeb />
+        </div>
+      )}
+      {status === Status.idle && (
+        <div className={styles["grid"]}>
+          {characters!.data.results.map((character) => {
+            const { id, name } = character;
+            return (
+              <Avatar
+                key={id}
+                id={id}
+                name={name}
+                thumbnail={
+                  character.thumbnail.path + "." + character.thumbnail.extension
+                }
+              />
+            );
+          })}
+        </div>
+      )}
+      {status === Status.error && "error "}
+    </Collections>
+  );
+};
+export default Characters;
