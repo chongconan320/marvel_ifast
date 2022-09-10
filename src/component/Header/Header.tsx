e8ff211f8fed5a5bfaa2dc8c19fde216099ca99f
Chong <chongconan320@gmail.com>
2022-09-10 15:19:28 +0800

    Transition on Header

--- src/component/Header/Header.tsx
+++ src/component/Header/Header.tsx
@@ -8,11 +8,10 @@ import { ReactComponent as ImageMarvelLogo } from "assets/images/marvel_logo.svg
 import NavButton from "./NavButton";
 
 // Import library
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link, useLocation } from "react-router-dom";
 import Hamburger from "./Hamburger";
 import Overlay from "./Overlay";
-import path from "path";
 
 const navigation = [
   {
@@ -29,15 +28,42 @@ const navigation = [
   },
 ];
 
+const TRANSITION_TRIGGER = 30;
+
 const Header = () => {
   const { pathname } = useLocation();
   const [isExpanded, setIsExpanded] = useState(false);
+  const headerRef = useRef<HTMLElement>(null);
+  const lastScrollY = useRef(0);
+
+  useEffect(() => {
+    const headerEl = headerRef.current;
+    if (headerEl === null) return;
+    const onScroll = () => {
+      if (
+        lastScrollY.current - window.scrollY > TRANSITION_TRIGGER ||
+        window.scrollY === 0
+      ) {
+        headerEl.style.transform = "";
+      }
+      if (window.scrollY - lastScrollY.current > TRANSITION_TRIGGER) {
+        headerEl.style.transform = "translateY(-100%)";
+      }
+      lastScrollY.current = window.scrollY;
+    };
+
+    document.addEventListener("scroll", onScroll);
+
+    return () => {
+      document.removeEventListener("scroll", onScroll);
+    };
+  }, []);
 
   const onToggle = () => {
     setIsExpanded((prev) => !prev);
   };
   return (
-    <header className={styles["header"]}>
+    <header className={styles["header"]} ref={headerRef}>
       <Overlay isExpanded={isExpanded}>
         <div className={styles["overlay"]}>
           <ul
